c2b84cb5e0895892ecb93b07483a793a79923f49
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-04-22 09:23:27 -0500

    Results make sense, so far no further filter was used. If filter inside mask wanted, run mask twice on same input sample

--- simulation.py
+++ simulation.py
@@ -1,5 +1,8 @@
 import numpy as np
 import numpy.linalg as la
+from random import randint
+from numpy.random import normal
+import math
 
 def mask(dimensions, porosity):
     """
@@ -10,11 +13,16 @@ def mask(dimensions, porosity):
     returns:
         2D-array mask with set porosity via booleans (0 = false, 1 = true)
     """
-    return
+    for i in range(len(dimensions)):
+        for j in range(len(dimensions[i])):
+            if randint(0,100) < porosity:
+                dimensions[i][j] = 1
+
+    return dimensions
 
 #required dimensions
 mask_size = np.zeros((1000,1000))
-mask_porosity = ""
+mask_porosity = 74 #https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7410795/ modified mask
 n_iteration = 1000
 
 
@@ -24,10 +32,15 @@ test = mask(mask_size, mask_porosity)
 #use gaussian random selection to simulate saliva burst from mouth
 n_true = 0
 n_false = 0
+
 for _ in range(n_iteration):
     #gaussian each time iteration is run
-    if test[x,y] == 0:
+    #parameters: mean, stdev, size
+    x = math.floor(normal(len(test)/2, len(test)/100))
+    y = math.floor(normal(len(test)/2, len(test)/100))
+    if test[x][y] == 0:
         n_false += 1
     else:
         n_true += 1
 
+print(n_true, n_false)
