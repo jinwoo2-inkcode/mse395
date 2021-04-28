86a1248c2da4b04590aab7be725c0dc35eb027e8
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-04-27 20:53:33 -0500

    added cloth sample porosity simulation

--- simulation.py
+++ simulation.py
@@ -13,20 +13,53 @@ def mask(dimensions, porosity):
     returns:
         2D-array mask with set porosity via booleans (0 = false, 1 = true)
     """
-    for i in range(len(dimensions)):
-        for j in range(len(dimensions[i])):
-            if randint(0,100) < porosity:
-                dimensions[i][j] = 1
+    mask_sample = dimensions.copy()
+    for i in range(len(mask_sample)):
+        for j in range(len(mask_sample[i])):
+            if randint(0,1000)/10 < porosity:
+                mask_sample[i][j] = 1
 
-    return dimensions
+    return mask_sample
+
+def spray_simulation(test):
+    """
+    use gaussian random selection to simulate saliva burst from mouth
+    args:
+        test: mask sample
+    returns:
+        Simulated porosity of sample
+    """
+    n_iteration = 1000
+    n_true = 0
+    n_false = 0
+    for _ in range(n_iteration):
+        #gaussian each time iteration is run
+        #parameters of normal: mean, stdev
+        x = math.floor(normal(len(test)/2, len(test)/100))
+        y = math.floor(normal(len(test)/2, len(test)/100))
+        if test[x][y] == 0:
+            n_false += 1
+        else:
+            n_true += 1
+    return n_true/(n_true+n_false)
 
 #required dimensions
 mask_size = np.zeros((1000,1000))
 mask_porosity = 74 #https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7410795/ modified mask
 filter_porosity = 70
 
+"""
+Cloth (no filter)	    	14.29375
+Cloth (coffee filter)		0.4875
+Cloth (paper towel)	    	1.26875
+"""
+
+cloth_filter = ["Cloth (no filter)", "Cloth (coffee filter)", "Cloth (paper towel)"]
+cloth_samples = [85.706, 99.5125, 98.73125]
+
+
 filter_object =  mask(np.zeros((1000,1000)), filter_porosity)
-n_iteration = 1000
+n_iteration = 5000
 
 
 #execution
@@ -34,19 +67,13 @@ test1 = mask(mask_size, mask_porosity)
 test2 = mask(filter_object, mask_porosity)
 
 test = test1 #type of mask used
+cloth_tests = []
 
-#use gaussian random selection to simulate saliva burst from mouth
-n_true = 0
-n_false = 0
+for i in cloth_samples:
+    cloth_tests.append(mask(mask_size, i))
 
-for _ in range(n_iteration):
-    #gaussian each time iteration is run
-    #parameters of normal: mean, stdev
-    x = math.floor(normal(len(test)/2, len(test)/100))
-    y = math.floor(normal(len(test)/2, len(test)/100))
-    if test[x][y] == 0:
-        n_false += 1
-    else:
-        n_true += 1
+for j in range(len(cloth_tests)):
+    print(cloth_filter[j], spray_simulation(cloth_tests[j]))
 
-print(n_true, n_false, n_true/(n_true+n_false))
+# for _ in range(5):
+#     print(spray_simulation(mask(mask_size, 85)))
